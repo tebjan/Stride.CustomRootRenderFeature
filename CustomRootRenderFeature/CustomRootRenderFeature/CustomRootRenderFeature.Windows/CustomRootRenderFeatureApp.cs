04b7da9b35769351a6eeebaf7c3651c33085ee9a
tebjan <tebjan@vvvv.org>
2018-08-17 21:04:45 +0200

    created basic game template

---
+++ CustomRootRenderFeature/CustomRootRenderFeature/CustomRootRenderFeature.Windows/CustomRootRenderFeatureApp.cs
@@ -0,0 +1,15 @@
+using Xenko.Engine;
+
+namespace CustomRootRenderFeature
+{
+    class CustomRootRenderFeatureApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
